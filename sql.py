abeef612d395c1dc0f11027429fb639f1724a1a0
Ashutosh@Pandey <112745204+Ashuto321@users.noreply.github.com>
2025-11-22 17:50:29 +0530

    Update sql.py

--- sql.py
+++ sql.py
@@ -1,35 +1,24 @@
 import sqlite3
 
-## Connectt to SQlite
-connection=sqlite3.connect("student.db")
+connection = sqlite3.connect("student.db")
+cursor = connection.cursor()
 
-# Create a cursor object to insert record,create table
+cursor.execute("""
+CREATE TABLE STUDENT(
+    NAME VARCHAR(25),
+    CLASS VARCHAR(25),
+    SECTION VARCHAR(25),
+    MARKS INT
+)
+""")
 
-cursor=connection.cursor()
+cursor.execute("INSERT INTO STUDENT VALUES('Ashutosh','Gen ai','A',90)")
+cursor.execute("INSERT INTO STUDENT VALUES('Anupam','DGen ai','B',100)")
+cursor.execute("INSERT INTO STUDENT VALUES('Evanjilin','powebi','A',86)")
+cursor.execute("INSERT INTO STUDENT VALUES('Nidhi','webDEVOPers','A',50)")
+cursor.execute("INSERT INTO STUDENT VALUES('Parshvi','powebi','A',35)")
 
-## create the table
-table_info="""
-Create table STUDENT(NAME VARCHAR(25),CLASS VARCHAR(25),
-SECTION VARCHAR(25),MARKS INT);
-
-"""
-cursor.execute(table_info)
-
-## Insert Some more records
-
-cursor.execute('''Insert Into STUDENT values('Ashutosh','Gen ai','A',90)''')
-cursor.execute('''Insert Into STUDENT values('Anupam','DGen ai','B',100)''')
-cursor.execute('''Insert Into STUDENT values('Evanjilin','powebi','A',86)''')
-cursor.execute('''Insert Into STUDENT values('Nidhi','webDEVOPers','A',50)''')
-cursor.execute('''Insert Into STUDENT values('Parshvi','powebi','A',35)''')
-
-## Disspaly ALl the records
-
-print("The isnerted records are")
-data=cursor.execute('''Select * from STUDENT''')
-for row in data:
-    print(row)
-
-## Commit your changes int he databse
 connection.commit()
 connection.close()
+
+print("Student DB Created!")
